e4c6813922d6520ede161e5a0383533e582bbb69
subsa <subsauceday@gmail.com>
2026-03-08 19:34:30 -0400

    Working C3

--- CPSAT.py
+++ CPSAT.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Any
 from dataclasses import dataclass
 import Json_departments_to_server_writer
-import Course_json_string_retriever
+from Course_json_string_retriever import Course_json_retriever
 from LabsOrTutorials import LabsOrTutorials
 from Lectures import Lectures
 from Sections import Sections
@@ -11,13 +11,16 @@ from Course import Course
 
 json_departments_to_server_writer = Json_departments_to_server_writer
 department_json_path = json_departments_to_server_writer.get_files()
+del json_departments_to_server_writer
+print(department_json_path)
 
-requested_courses = list[str]
-requested_courses.append(input("Ok nice, now give your course names and type '-1' to leave"))
-
+requested_courses = [input("Ok nice, now give your course names and type '-1' to leave")]
 while (requested_courses[-1] != "-1"):
-    requested_courses.append(input)
+    requested_courses.append(input("List another or type '-1' to leave"))
+del(requested_courses[-1])
+print(requested_courses)
 
-Course_json_string_retriever = Course_json_string_retriever(department_json_path, requested_courses)
-department_json_retriever.get_department_jsons()
+Course_json_retriever = Course_json_retriever(department_json_path, requested_courses)
+course_jsons = Course_json_retriever.get_course_jsons()
+del Course_json_retriever
         
